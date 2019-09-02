7b42416599bedaa7aafaf834d72068cf24611d6b
Mohamed el fadhel el hachemi <mohamedelfadhel.elhachemi@gmail.com>
2019-09-02 14:58:18 +0200

    added storybook

---
+++ example/stories/index.stories.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { Header } from 'phoenix-ui-kit'
+
+storiesOf('Header', module).add('to Storybook', () => <Header />)
